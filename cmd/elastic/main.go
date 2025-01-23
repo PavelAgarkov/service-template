afc09c9b7ffcd6158d4ba5e9bd8ee2143fb2b16f
agarkov.pavel3 <agarkov.pavel.ru1@gmail.com>
2025-01-23 16:25:15 +0600

    elastic facade

--- cmd/elastic/main.go
+++ cmd/elastic/main.go
@@ -2,74 +2,100 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"go.uber.org/zap"
 	"log"
-	"strings"
+	"os"
+	"os/signal"
+	"service-template/application"
+	"service-template/pkg"
+	"syscall"
 
 	"github.com/elastic/go-elasticsearch/v8"
-	"github.com/elastic/go-elasticsearch/v8/esapi"
 )
 
 func main() {
-	// 1. Настраиваем соединение
-	cfg := elasticsearch.Config{
-		Addresses: []string{"http://localhost:9200"},
-		Username:  "elastic",
-		Password:  "elasticpassword",
-		// Если работаете по HTTPS/TLS — нужно добавить настройки для сертификатов (TLS), см. документацию.
-	}
+	logger := pkg.NewLogger(pkg.LoggerConfig{ServiceName: "elastic", LogPath: "logs/app.log"})
+	father, cancel := context.WithCancel(context.Background())
+	father = pkg.LoggerWithCtx(father, logger)
+	defer cancel()
 
-	es, err := elasticsearch.NewClient(cfg)
+	logger.Info("config initializing")
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+	defer signal.Stop(sig)
+
+	go func() {
+		<-sig
+		logger.Info("Signal received. Shutting down server...")
+		cancel()
+	}()
+
+	app := application.NewApp(logger)
+	defer app.Stop()
+	defer app.RegisterRecovers(logger, sig)()
+
+	es, err := pkg.NewElasticFacade(
+		elasticsearch.Config{
+			Addresses: []string{"http://localhost:9200"},
+			Username:  "elastic",
+			Password:  "elasticpassword",
+			// Если работаете по HTTPS/TLS — нужно добавить настройки для сертификатов (TLS), см. документацию.
+		}, logger)
 	if err != nil {
-		log.Fatalf("Ошибка создания клиента: %s", err)
+		logger.Fatal("Ошибка создания фасада Elasticsearch: %s", zap.Error(err))
 	}
 
-	// 2. Индексируем документ
-	docBody := `{"title": "Hello from Go", "views": 42}`
-
-	req := esapi.IndexRequest{
-		Index:      "test-index",               // Название индекса
-		DocumentID: "1",                        // Опционально можем указать ID
-		Body:       strings.NewReader(docBody), // Сам JSON документа
-		Refresh:    "true",                     // Обновить индекс сразу, чтобы поиск видел изменения
+	// 1. Индексируем документ
+	doc := map[string]interface{}{
+		"title": "Hello from Go 2",
+		"views": 47,
 	}
 
-	res, err := req.Do(context.Background(), es)
+	err = es.IndexDocument(father, "test-index", "2", doc)
 	if err != nil {
 		log.Fatalf("Ошибка при индексации: %s", err)
 	}
-	defer res.Body.Close()
-
-	if res.IsError() {
-		log.Fatalf("Elasticsearch вернул ошибку индексации: %s", res.String())
-	}
 	fmt.Println("Документ успешно проиндексирован.")
 
-	// 3. Выполняем поиск
-	searchBody := `{
-	  "query": {
-	    "match": {
-	      "title": "Go"
-	    }
-	  }
-	}`
+	// 2. Выполняем поиск
+	searchQuery := map[string]interface{}{
+		"match": map[string]interface{}{
+			"title": "Go",
+		},
+	}
 
-	searchRes, err := es.Search(
-		es.Search.WithContext(context.Background()),
-		es.Search.WithIndex("test-index"),
-		es.Search.WithBody(strings.NewReader(searchBody)),
-		es.Search.WithTrackTotalHits(true),
-		es.Search.WithPretty(), // форматированный JSON-ответ
-	)
+	m, err := es.Search(father, "test-index", searchQuery)
 	if err != nil {
-		log.Fatalf("Ошибка при поиске: %s", err)
-	}
-	defer searchRes.Body.Close()
-
-	if searchRes.IsError() {
-		log.Fatalf("Ошибка в поисковом запросе: %s", searchRes.String())
+		log.Fatalf("Ошибка при выполнении поиска: %s", err)
 	}
 
-	fmt.Println("Результат поиска:")
-	fmt.Println(searchRes)
+	// Логирование результатов поиска
+	logger.Info("search result", zap.Any("result", m))
+
+	// 3. Выполняем агрегацию
+	aggregation := map[string]interface{}{
+		"average_views": map[string]interface{}{
+			"avg": map[string]interface{}{
+				"field": "views",
+			},
+		},
+	}
+
+	aggResult, err := es.Aggregate(father, "test-index", aggregation)
+	if err != nil {
+		log.Fatalf("Ошибка при выполнении агрегации: %s", err)
+	}
+
+	// Вывод результатов агрегации
+	prettyAggJSON, err := json.MarshalIndent(aggResult, "", "  ")
+	if err != nil {
+		log.Fatalf("Ошибка форматирования JSON агрегации: %s", err)
+	}
+	fmt.Println("Результат агрегации:")
+	fmt.Println(string(prettyAggJSON))
+
+	<-father.Done()
+	logger.Info("application exited gracefully")
 }
